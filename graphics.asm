88288a16d64104af7a6bf972d6d817c6fac1932a
gggraph <62741099+gggraph@users.noreply.github.com>
2021-05-03 14:34:16 +0200

    Update graphics.asm

--- graphics.asm
+++ graphics.asm
@@ -1,48 +1,16 @@
 ; a simple graphic library 
-;-------------------- CALL CONVENTION -----------------
-push 10
-push 10
-push 50
-push 20
-push 30
-push 80
-call DRAW_TRIANGLE
-;-----------------------------------------------------
+
 
 VERT_LINE:
-;[bp+8] ; x1
-;[bp+6] ; y1
-;[bp+4] ; y0 
+; y0 y1; x 
+; +8 +6 +4
 
 push bp
 mov bp,sp 
 
 .loop:
 ; while ( y0 < y1 ) y++ draw(xy)
-mov ax, [bp+4]
-cmp ax, [bp+6]
-ja .endloop
-; draw pixel 
-mov ah, 0ch
-mov bh, 0
-mov al, 50 ; color
-mov cx, [bp +8 ] ; start pos x
-mov dx, [bp +4 ] ; start pos y
-int 10h ;     
-; inc y0
-inc word [bp+4]
-jmp .loop
-.endloop:
-pop bp
-ret 6 ; clear stack here
-
-HOR_LINE:
-
-push bp
-mov bp,sp 
-.loop:
-; while ( x0 < x1 ) x++ draw(xy)
-mov ax, [bp+4]
+mov ax, [bp+8]
 cmp ax, [bp+6]
 ja .endloop
 ; draw pixel 
@@ -53,7 +21,31 @@ mov cx, [bp +4 ] ; start pos x
 mov dx, [bp +8 ] ; start pos y
 int 10h ;     
 ; inc y0
-inc word [bp+4]
+inc word [bp+8]
+jmp .loop
+.endloop:
+pop bp
+ret 6 ; clear stack here
+
+HOR_LINE:
+; x0 x1; y 
+; +8 +6 +4
+push bp
+mov bp,sp 
+.loop:
+; while ( x0 < x1 ) x++ draw(xy)
+mov ax, [bp+8]
+cmp ax, [bp+6]
+ja .endloop
+; draw pixel 
+mov ah, 0ch
+mov bh, 0
+mov al, 50 ; color
+mov cx, [bp +8 ] ; start pos x
+mov dx, [bp +4 ] ; start pos y
+int 10h ;     
+; inc y0
+inc word [bp+8]
 jmp .loop
 .endloop:
 pop bp
@@ -206,7 +198,73 @@ push word [bp+12]
 call BRESENHAM_LINE
 
 ;add sp, 24 ; ----------> we dont need to clear args here cause bresenham already clear the arg . we need to clear it if no clearing during process
+pop bp
 ret 12 ; clear stack 
 
+DRAW_RECTANGLE:
 
+push bp; 
+mov bp, sp
+
+; arg : x y w h  ()
+; x : +10
+; y : +8
+; w : +6
+; h : +4
+
+; draw x y to x+w y ( HOR LINE)
+; draw x y+h to x+w y+h (HOR LINE)
+
+
+; HOR LINE push order : x0,x1,y
+
+mov ax, [bp+10]
+push ax
+add ax, [bp+6]
+push ax
+push word[bp+8]
+call HOR_LINE
+
+mov ax, [bp+10]
+push ax
+add ax, [bp+6]
+push ax
+mov ax, [bp+8]
+add ax, [bp+4]
+push ax
+call HOR_LINE
+
+
+
+; draw x y to x y+h ( VERT LINE)
+; draw x+w y to x+w y+h (VERT LINE)
+; VERT LINE push order : y0,y1,x
+mov ax, [bp+8]
+push ax
+add ax, [bp+4]
+push ax
+push word [bp+10]
+call VERT_LINE
+
+mov ax, [bp+8]
+push ax
+add ax, [bp+4]
+push ax
+mov ax, [bp+10]
+add ax, [bp+6]
+push ax
+call VERT_LINE
+
+
+pop bp
+ret 8
+
+CLR_SCREEN: 
+; bh is the color
+mov ah, 06h    ; Scroll up function
+xor al, al     ; Clear entire screen
+xor cx, cx    ; Upper left corner CH=row, CL=column
+mov dx, 184FH  ; lower right corner DH=row, DL=column 
+int  10H
+ret
 
