6bd6deff30e6741f4da85eeff912e6af1793b17f
gggraph <62741099+gggraph@users.noreply.github.com>
2021-06-20 12:45:39 +0200

    adding decoding 8b color palette + dithering

--- bitmap.asm
+++ bitmap.asm
@@ -1,18 +1,18 @@
-;; we need to add scale function ( so a pixel mult ratio)
+; --------------------------------
+; adding  croping parameters
+;---------------------------------
+PRINT_BITMAP:
 
-PRINT_BITMAP_16b24b:
-
-; [bp+12] un petit kiff pour les mouvements de couleurs
-;[bp+10]  is scaling
-;[bp+8]  is x offset pos
-;[bp+6]  is y offset pos
+; [bp+1] un petit kiff pour les mouvements de couleurs
+;[bp+12]  is scaling
+;[bp+10]  is x offset pos
+;[bp+8]  is y offset pos
 ;[bp+4]  is bitmap binary pointer
 
 push bp
 mov bp, sp
  
-xor edi, edi
-mov di, [bp+4]
+mov edi,  [bp+4]
 
 ; bmp size is at  [esi]
 ; bit ptr is at   [esi+10]
@@ -20,71 +20,69 @@ mov di, [bp+4]
 
 
 
+; get bit array offset
+cmp word[edi+28], 8  
+jg .16bp
+; is at [edi+46](biClrUsed) * 4 + 54 
+mov eax, [edi+46]
+shl eax, 2 ; mult by 4
+add eax, 54
+push eax
+jmp .af
 
+.16bp:
 mov eax, 54 ; this is bitarray ptr. i mean it is always 54 for 16b and 24b
 push eax ; the counter at [bp-4]
 
+.af:
+ ; number of byte is word[edi+28]
+xor eax, eax
 mov ax, word[edi+28]
-cmp ax, 16
-je .ea
-cmp ax, 24
-je .eb
-.ea:
-push 2
-jmp .n
-.eb:
-push 3
-jmp .n
-; here i could do handle erro
-.n:
+; divide it per 8 
+shr ax, 3
+push ax
 
 ; proccess padding. 
 
-mov eax,  dword [edi+18] 
+mov eax,  dword [edi+18]  ; load bmp with in eax
 xor ecx, ecx
 mov cx, [bp-6]
 mul ecx
-and eax, ecx ; we can also use test 
+mov ecx, 3
+and eax, ecx 
 cmp eax, 0
-je .next
+je .condb
 
-.conda:
 mov edx, 4
 sub edx, eax
 push dx
 jmp .next
 .condb:
-xor edx, edx
-push dx
+push word 0
 .next:
 
 
-;mov eax, 0x18 ; 
-;cmp al, [edi+28]
-;jne .end ; this is not a 24 bit depth bitmap; nop
-
 ; cx and dx has to be respectively x and y , bl is color 
 xor edx, edx
-mov dx, [bp+6]
+mov dx, [bp+8]
 add edx, dword[edi+22] ; i got 0 but it should be 16 i dont understant
 dec edx
 
 
-
 .loopY:
 xor eax, eax
-mov ax,  [bp+6] ; should clear either low or high then 
+mov ax,  [bp+8] ; should clear either low or high then 
 
 cmp edx, eax
 jl .end
 xor ecx, ecx
-mov cx, [bp+8]
+mov cx, [bp+10]
 
 	
 	.loopX:
 	
 	xor eax, eax
-	mov ax,  [bp+8] ; should clear either low or high then 
+	mov ax,  [bp+10] ; should clear either low or high then 
 	add eax,  dword [edi+18] ; this is an int i guess
 	cmp ecx, eax
 	je .endX
@@ -94,52 +92,69 @@ mov cx, [bp+8]
 	; we will do some hack here to convert to grayscale:  
 	xor ebx, ebx
 	mov esi, dword[bp-4]; the counter
-	mov bl,  [edi+esi+1] ; 
-	shr bl, 5
-	add bl, [bp+12]; -------------------- : normal grayscale10h
-	;mov bh, 10h
-	add si, [bp-6] ; ca devrait etre trois . mais je n'obtiens pas trois .
-	mov dword[bp-4], esi
+	
 	
 	;bmp scaling ( we could use fast scaling using multiple of 2 . but lets go )
+
+	
+	; ---------------------------- GET THE COLOR DEPENDING OF BIT DEPTH ( read colore palette or not )  ----------------------------
+	cmp word[edi+28], 8  
+	jg .nocolortable
+	; r g b is at file + 54 +  [edi+esi+1] * 4
+	xor eax, eax
+	mov al, [edi+esi+1] ; read color table index
+	shl al, 2 ; mult by 4
+	add eax, 54 ; add 54 b off 
+	mov bl, [edi+eax+1]
+	jmp .convertgrayscale
+
+	.nocolortable:
+	mov bl,  [edi+esi+1] 
+
+	.convertgrayscale:
+	; ---------------------------------------------------------------------------------------------------------------
+	shr bl, 5
+	add bl, [bp+14]; -------------------- : normal grayscale10h
+	;mov bh, 10h
+	add si, [bp-6] ; increment array ptr (by one, two or three)
+	mov dword[bp-4], esi
 
 	push edi
 	push ecx
 	push edx
 
-	
 	push 10; dither a
 	push 2; dither b
 	xor edi, edi
 	mov eax, ecx
-	mov di, [bp + 8]
+	mov di, [bp + 10]
 	sub eax, edi
 	xor edi, edi
-	mov di, [bp+10] ; scale
+	mov di, [bp+12] ; scale
 	push edx
 	mul edi
 	pop edx
 	xor edi, edi
-	mov di, [bp + 8]
+	mov di, [bp + 10]
 	add eax, edi
 	push ax
 
 	xor edi, edi
 	mov eax, edx
-	mov di, [bp +6]
+	mov di, [bp +8]
 	sub eax, edi
 	xor edi, edi
-	mov di, [bp+10] ; scale
+	mov di, [bp+12] ; scale
 	push edx
 	mul edi
 	pop edx
 	xor edi, edi
-	mov di, [bp + 6]
+	mov di, [bp + 8]
 	add eax, edi
 	push ax
 
 	xor edi, edi
-	mov di, [bp+10] ; scale
+	mov di, [bp+12] ; scale
 	push di
 	push di
 
@@ -172,5 +187,4 @@ add sp, 8
 pop bp
 ret 10
 
-;; this is an alone field to concat bmp & testing stuff
 mbmp:
